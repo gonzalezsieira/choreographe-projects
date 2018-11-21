b3fee1a3c58bad2a447adbb2033fcb2c429b971d
Adrián González Sieira <adrian.gonzalez.sieira@gmail.com>
2018-11-21 14:28:05 +0100

    [Pepper] Added hmi_pepper_simple

---
+++ hmi_pepper_simple/translations/translation_nl_NL.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello! I hope you are doing great today Daniel</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello! I hope you are doing great today Daniel</translation>
+        </message>
+        <message>
+            <source>goedemorgen, bedankt dat je hier bent</source>
+            <comment>Text</comment>
+            <translation type="obsolete">goedemorgen, bedankt dat je hier bent</translation>
+        </message>
+        <message>
+            <source>hallo, ik heet pepper, wat leuk om jou te ontmoeten</source>
+            <comment>Text</comment>
+            <translation type="obsolete">hallo, ik heet pepper, wat leuk om jou te ontmoeten</translation>
+        </message>
+        <message>
+            <source>Hoi Robert, ik hoop dat je een leuke dag hebt</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hoi Robert, ik hoop dat je een leuke dag hebt</translation>
+        </message>
+        <message>
+            <source>Hallo ik ben Robert, ik hoop dat je een leuke dag hebt</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo ik ben Robert, ik hoop dat je een leuke dag hebt</translation>
+        </message>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo</translation>
+        </message>
+        <message>
+            <source>Hallo Robert</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo Robert</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Meet Robert and say Hello/Animated Say</name>
+        <message>
+            <source>Hallo Robert</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo Robert</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hallo Robert, ik hoop dat je een geweldige tijd hebt</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hallo Robert, ik hoop dat je een geweldige tijd hebt</translation>
+        </message>
+    </context>
+</TS>
